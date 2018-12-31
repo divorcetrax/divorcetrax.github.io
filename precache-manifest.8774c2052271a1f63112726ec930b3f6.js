3916123065f9f011b1f469b47ad16b5c847a015e
Dylan Kario <drk2130@columbia.edu>
2018-12-31 15:26:24 -0800

    Updates

---
+++ precache-manifest.8774c2052271a1f63112726ec930b3f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d1ed38b1b77ba3ee676",
+    "url": "/static/js/main.5d1ed38b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5d1ed38b1b77ba3ee676",
+    "url": "/static/css/main.2b0db90d.chunk.css"
+  },
+  {
+    "revision": "97780082799da3f735e1940b8a3e4990",
+    "url": "/index.html"
+  }
+];
